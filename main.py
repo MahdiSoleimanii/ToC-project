40e15ebcdd9c62042b7a040f8e75f9006ead0ea9
Mahdi Soleimani <mahdisoleimani101@gmail.com>
2023-02-09 23:10:48 +0330

    new testcase added and initialized

--- main.py
+++ main.py
@@ -19,4 +19,6 @@ testfa12 = DFA(testcases[11]['states'], testcases[11]['alphabet'], testcases[11]
 testfa13 = DFA(testcases[12]['states'], testcases[12]['alphabet'], testcases[12]['start_state'], testcases[12]['accepting_states'], testcases[12]['transition_function'])
 testfa14 = DFA(testcases[13]['states'], testcases[13]['alphabet'], testcases[13]['start_state'], testcases[13]['accepting_states'], testcases[13]['transition_function'])
 testfa15 = DFA(testcases[14]['states'], testcases[14]['alphabet'], testcases[14]['start_state'], testcases[14]['accepting_states'], testcases[14]['transition_function'])
+testfa16 = DFA(testcases[15]['states'], testcases[15]['alphabet'], testcases[15]['start_state'], testcases[15]['accepting_states'], testcases[15]['transition_function'])
 
+print(testfa15.minimize() == testfa16)
